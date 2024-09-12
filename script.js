9c58c6de0f4d78ea5f9f62a5f2c33f8ff751c0c4
HOTSAUCE234232323 <82414668+HOTSAUCE234232323@users.noreply.github.com>
2024-09-12 12:27:42 -0400

    1 fix #2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const div = document.createElement("div")
 const text = document.createTextNode("Hello")
+div.style.backgroundColor = "Grey"
 div.appendChild(text)
 document.getElementById("videos").appendChild(div)
